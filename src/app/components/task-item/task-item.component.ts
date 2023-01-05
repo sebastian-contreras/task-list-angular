a0154e29f26faedb954b368db6a5ef09940e565f
Sebastian Contreras <Sebasfac2017@gmail.com>
2023-01-05 16:36:11 -0300

    Estilo Items

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
-import {Task} from '../../Task';
-import {TASKS} from '../../mock-task'
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-task'
+
+
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent {
-   @Input() task: Task = TASKS[0]
-
+  @Input() task: Task = TASKS[0]
+  faTimes = faTimes;
 
 }
